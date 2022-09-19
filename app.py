3139d4ac050f46cf6e3930ef76cb86823eed05df
Diego Lanciotti <aaadiego502@gmail.com>
2022-09-19 12:31:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,14 @@
 import PySimpleGUI as sg                      # Parte 1 - Importamos
 
 # Definimos el contenido de la ventana
-layout = [  [sg.Text("Como te llamas?")],     # Parte 2 - El Layout
-            [sg.Input()],
-            [sg.Button('Ok')] ]
+layout = [  [sg.Text("Bienvenidos al curso de intefaz gráfica con Python.\n\n🚧 Cualquier duda podés escribir en el foro.")],     # Parte 2 - El Layout
+        ]
 
 # Creamos la ventana
-window = sg.Window('Titulo de la ventana', layout)  # Parte 3 - Definimos la ventana
+window = sg.Window('Titulo de la ventana', layout)  # Parte 2 - Definimos la ventana
                                                 
 # Dibujamos la ventana y leemos los eventos
-event, values = window.read()                 # Parte 4 - Loop de eventos
-
-# Hacemos algo con la información obtenida
-print('Hola', values[0], "! Gracias por sumarte al curso de PySimpleGUI")
+event, values = window.read()                 # Parte 3 - Loop de eventos
 
 # Cerramos la ventana
-window.close()                                # Parte 5 - Cerramos la ventana
+window.close()                                # Parte 4 - Cerramos la ventana
